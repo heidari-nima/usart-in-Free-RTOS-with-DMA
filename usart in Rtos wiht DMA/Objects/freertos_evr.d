b3f8121f43ea7273bf8e726a86cbbe4046c70be1
Nima Heidari <79926326+heidari-nima@users.noreply.github.com>
2024-06-18 04:55:07 +0330

    Add files via upload

---
+++ usart in Rtos wiht DMA/Objects/freertos_evr.d
@@ -0,0 +1,13 @@
+./objects/freertos_evr.o: \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\CMSIS\RTOS2\FreeRTOS\Source\freertos_evr.c \
+  d:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\stdlib.h RTE\RTOS\FreeRTOSConfig.h \
+  RTE\_Target_1\RTE_Components.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm3.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h
---
+++ usart in Rtos wiht DMA/Objects/main.d
@@ -0,0 +1,25 @@
+./objects/main.o: main.c \
+  D:\Users\Nova\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm3.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\FreeRTOS.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\stddef.h RTE\RTOS\FreeRTOSConfig.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\projdefs.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\portable.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\deprecated_definitions.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\portable\GCC\ARM_CM3\portmacro.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\mpu_wrappers.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\task.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\list.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\semphr.h \
+  D:\Users\Nova\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\11.1.0\Source\include\queue.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\string.h \
+  d:\Keil_v5\ARM\ARMCLANG\include\stdio.h
